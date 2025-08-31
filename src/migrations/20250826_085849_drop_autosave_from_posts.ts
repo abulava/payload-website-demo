fba87efff6d8c081b3b966745e27a6bff8a43998
Andrey Bulava <abulava@gmail.com>
2025-08-31 16:48:57 +0300

    Cleanup unused declarations.

--- src/migrations/20250826_085849_drop_autosave_from_posts.ts
+++ src/migrations/20250826_085849_drop_autosave_from_posts.ts
@@ -1,12 +1,12 @@
 import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
 
-export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+export async function up({ db }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
    DROP INDEX "_posts_v_autosave_idx";
   ALTER TABLE "_posts_v" DROP COLUMN "autosave";`)
 }
 
-export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+export async function down({ db }: MigrateDownArgs): Promise<void> {
   await db.execute(sql`
    ALTER TABLE "_posts_v" ADD COLUMN "autosave" boolean;
   CREATE INDEX "_posts_v_autosave_idx" ON "_posts_v" USING btree ("autosave");`)
--- src/migrations/20250826_122833_drop_autosave_from_pages.ts
+++ src/migrations/20250826_122833_drop_autosave_from_pages.ts
@@ -1,12 +1,12 @@
 import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
 
-export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+export async function up({ db }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
    DROP INDEX "_pages_v_autosave_idx";
   ALTER TABLE "_pages_v" DROP COLUMN "autosave";`)
 }
 
-export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+export async function down({ db }: MigrateDownArgs): Promise<void> {
   await db.execute(sql`
    ALTER TABLE "_pages_v" ADD COLUMN "autosave" boolean;
   CREATE INDEX "_pages_v_autosave_idx" ON "_pages_v" USING btree ("autosave");`)
