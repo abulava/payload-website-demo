d8796af79c92aa36992187a2dc8928ecc9566647
Andrey Bulava <abulava@gmail.com>
2025-09-11 13:46:19 +0300

    @WIP.

--- src/collections/Users/index.ts
+++ src/collections/Users/index.ts
@@ -1,6 +1,26 @@
 import type { CollectionConfig } from 'payload'
 
 import { authenticated } from '../../access/authenticated'
+import { isSuperAdmin } from '../../access/superAdmin'
+import { tenantsArrayField } from '@payloadcms/plugin-multi-tenant/fields'
+
+const defaultTenantArrayField = tenantsArrayField({
+  tenantsArrayFieldName: 'tenants',
+  tenantsArrayTenantFieldName: 'tenant',
+  tenantsCollectionSlug: 'tenants',
+  arrayFieldAccess: {},
+  tenantFieldAccess: {},
+  rowFields: [
+    {
+      name: 'roles',
+      type: 'select',
+      defaultValue: ['tenant-viewer'],
+      hasMany: true,
+      options: ['tenant-admin', 'tenant-viewer'],
+      required: true,
+    },
+  ],
+})
 
 export const Users: CollectionConfig = {
   slug: 'users',
@@ -19,8 +39,30 @@ export const Users: CollectionConfig = {
   auth: true,
   fields: [
     {
+      admin: {
+        position: 'sidebar',
+      },
+      name: 'roles',
+      type: 'select',
+      defaultValue: ['user'],
+      hasMany: true,
+      options: ['super-admin', 'user'],
+      access: {
+        update: ({ req }) => {
+          return isSuperAdmin(req.user)
+        },
+      },
+    },
+    {
       name: 'name',
       type: 'text',
+    },
+    {
+      ...defaultTenantArrayField,
+      admin: {
+        ...(defaultTenantArrayField?.admin || {}),
+        position: 'sidebar',
+      },
     },
   ],
   timestamps: true,
